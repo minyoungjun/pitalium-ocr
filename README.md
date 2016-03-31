4c1ccf8dd5c8ce38a74d75cfa6aa77d6ae34e005
Minyoung Jun <minyoung9353@gmail.com>
2016-03-31 21:52:15 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+#OCR method for Pitalium explorer
+
+This is the OCR method for Pitalium.
+This method using Tesseract-ocr and Tess4j.
+Tesseract-ocr is open source OCR engine can be used in various OS.
+Tess4j is library for using Tesseract in java.
+##Prerequisite
+
+1. [Eclipse any version](https://eclipse.org/)
+2. [Tesseract](https://github.com/tesseract-ocr/tesseract)
+
+If you didn't install Tesseract, Eclipse will show error messages.
---
+++ src/tess4j/example/TesseractExample.java
@@ -0,0 +1,22 @@
+package tess4j.example;
+
+import java.io.File;
+import net.sourceforge.tess4j.*;
+
+public class TesseractExample {
+
+    public static void main(String[] args) {
+        File imageFile = new File("eurotext.png");
+        ITesseract instance = new Tesseract();  // JNA Interface Mapping
+        // ITesseract instance = new Tesseract1(); // JNA Direct Mapping
+
+        try {
+            String result = instance.doOCR(imageFile);
+            System.out.println(result);
+        } catch (TesseractException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+    
+    
+}
